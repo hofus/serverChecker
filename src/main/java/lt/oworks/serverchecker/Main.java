ccd230130084e4f6bfd1a3f2474b69c1aa7aca22
Ovidijus <ovidijus1989@gmail.com>
2013-05-15 23:48:04 +0300

    set up main class

--- src/main/java/lt/oworks/serverchecker/Main.java
+++ src/main/java/lt/oworks/serverchecker/Main.java
@@ -4,10 +4,13 @@ package lt.oworks.serverchecker;
  * Hello world!
  *
  */
-public class Main 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class Main {
+
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.err.println("Need settings file path");
+        } else {
+            System.out.println("Starting");
+        }
     }
 }
