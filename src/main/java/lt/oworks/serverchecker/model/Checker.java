cc73e2aba57ed1f6bcd3ba9283315dcf5ab531e2
narkeovi <narkeovi@LTNARKEOVI02.lt.dom>
2013-05-28 13:15:50 +0300

    add mail library

--- src/main/java/lt/oworks/serverchecker/model/Checker.java
+++ src/main/java/lt/oworks/serverchecker/model/Checker.java
@@ -1,8 +1,18 @@
 package lt.oworks.serverchecker.model;
 
+import java.util.TimerTask;
+import lt.oworks.serverchecker.model.settings.ServerSettings;
+
 /**
  *
  * @author Ovidijus
  */
-public class Checker {
+public class Checker extends TimerTask{
+    
+    private ServerSettings settings;
+
+    @Override
+    public void run() {
+        
+    }
 }
